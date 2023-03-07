64a7cc071f87318cc6e21fdbe52ab105b46783b5
naturalyuan <naturalyuan@tencent.com>
2023-03-07 12:01:59 +0800

    fix(TRTCScenesDemo/*): update TUICallKit demo“

---
+++ TRTCScenesDemo/TUICallKit-WX-Demo/pages/login/login.js
@@ -0,0 +1,48 @@
+import { genTestUserSig } from '../../debug/GenerateTestUserSig';
+Page({
+  data: {
+    userID: '',
+  },
+
+  onLoad() {
+
+  },
+
+  onShow() {
+
+  },
+
+  onBack() {
+    wx.navigateBack({
+      delta: 1,
+    });
+  },
+
+  bindInputUserID(e) {
+    this.setData({
+      userID: e.detail.value,
+    });
+  },
+
+  login() {
+    if (!this.data.userID) {
+      wx.showToast({
+        title: '名称不能为空',
+        icon: 'error',
+      });
+    } else {
+      wx.$globalData.userID = this.data.userID;
+      const { userSig } = genTestUserSig(wx.$globalData.userID);
+      wx.$globalData.userSig = userSig;
+      wx.CallManager.init({
+        sdkAppID: wx.$globalData.sdkAppID,
+        userID: wx.$globalData.userID,
+        userSig: wx.$globalData.userSig,
+        globalCallPagePath: 'TUICallKit/pages/globalCall/globalCall',
+      });
+      wx.switchTab({
+        url: '../index/index',
+      });
+    }
+  },
+});
